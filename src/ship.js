8d4a34d61b5c771358255a70d560abf4109f4d09
Romain Dardour <romain@hull.io>
2016-01-22 16:49:57 +0100

    Detect Login Ship presence in page

--- src/ship.js
+++ src/ship.js
@@ -1,14 +1,22 @@
 /* global Hull, $*/
 
+import detectShip from './lib/detect-ship';
+
 import cart from './shopify/cart';
 import identity from './shopify/identity';
 import track from './shopify/track';
 
 export default function start(element, deployment, hull) {
   const data = window.HullShopify || {};
-  identity(hull, data);
+
   cart(hull, data);
   track(hull, data);
+
+  detectShip(hull, 'hull-login').then(function(present) {
+    if (present) {
+      identity(hull, data);
+    }
+  });
 }
 
 if (Hull) {
