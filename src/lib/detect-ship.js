366e86029e431b905845427c39a59f73f5fbad4e
Stephane Bellity <stephane@hull.io>
2017-06-21 19:14:29 +0200

    Fix detect-ship

--- src/lib/detect-ship.js
+++ src/lib/detect-ship.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 
 export default function detectShip(hull, shipName) {
-  return hull.ready((h, me, platform)=>{
+  return hull.ready().then(({ app }) => {
     return _.some(
-      _.map(platform.deployments, (item)=> {
+      _.map(app.deployments, (item)=> {
         return item.ship.manifest_url.indexOf(`/${shipName}/manifest.json`) > 0;
       }));
   });
